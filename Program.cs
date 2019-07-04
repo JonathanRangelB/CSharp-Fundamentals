c9885aa23b0c105b7dec65b55d0c98966c7f5516
Jonathan Rangel <jonathanrangelb@gmail.com>
2019-07-04 17:11:38 -0500

    Se hicieron cambios en la clase principal para agregar objetos a arreglos e imprimirlos

--- Program.cs
+++ Program.cs
@@ -10,21 +10,36 @@ namespace FundamentosCSharp
             //Como los ultimos 2 valores son opcionales al constructor, los les puedo cambiar el orden, el valor que mando siempre va despues de :
             var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, ciudad:"Bogota", pais:"Colombia");
             escuela.TipoEscuela = TiposEscuela.Secundaria;
-            var curso1 = new Curso() {
+
+            //creamos un arreglo de objetos curso y los asignamos de difetenres maneras al arreglo
+            var cursos = new Curso[3];
+            cursos[0] = new Curso()
+            {
                 Nombre = "101"
             };
             var curso2 = new Curso() {
                 Nombre = "201"
             };
-            var curso3 = new Curso() {
+            cursos[1] = curso2;
+            cursos[2]= new Curso{
                 Nombre = "301"
             };
-            Console.WriteLine(escuela);
 
+            //imprimimos los valores de nuestros objetos
+            Console.WriteLine(escuela);
             System.Console.WriteLine("================================");
-            System.Console.WriteLine(curso1.Nombre + " , " + curso1.UniqueId);
-            System.Console.WriteLine($"{curso2.Nombre} , {curso2.UniqueId}");
-            System.Console.WriteLine(curso3);
+            imprimirCursos(cursos);
+        }
+
+        //Imprime todos los elementos del arreglo
+        private static void imprimirCursos(Curso[] cursos)
+        {
+            int contador = 0;
+            while (contador < cursos.Length)
+            {
+                Console.WriteLine($"Nombre {cursos[contador].Nombre}, ID {cursos[contador].UniqueId}");
+                contador++;
+            }
         }
     }
 }
