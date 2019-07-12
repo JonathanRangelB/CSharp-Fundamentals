cbee1f25ed3e3ebc414970ced6c13004ec62ce8a
Jonathan Rangel <JonathanRangelB@gmail.com>
2019-07-12 11:27:54 -0500

    Se agrego una interfaz con metodo y atributo, y se implemento en escuela y curso

--- Entidades/Curso.cs
+++ Entidades/Curso.cs
@@ -1,17 +1,25 @@
 using System;
 using System.Collections.Generic;
+using CoreEscuela.Util;
 
 namespace CoreEscuela.Entidades
 {
-    public class Curso: ObjetoEscuelBase
+    public class Curso: ObjetoEscuelBase, ILugar
     {
         public TiposJornada Jornada { get; set; }
         public List<Asignatura> Asignaturas{ get; set; }
         public List<Alumno> Alumnos{ get; set; }
         // public Curso() => UniqueId = Guid.NewGuid().ToString();
+        string ILugar.Direccion { get; set; }
 
         public Curso(){
             Alumnos = new List<Alumno>();
+        }
+
+        public void LimpiarLugar(){
+            Printer.DrawLine();
+            Console.WriteLine("Limpiando Establecimiento...");
+            Console.WriteLine($"Curso {Nombre} Limpio");
         }
     }
 }
--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using CoreEscuela.Util;
 
 namespace CoreEscuela.Entidades
 {
-    public class Escuela: ObjetoEscuelBase
+    public class Escuela : ObjetoEscuelBase, ILugar
     {
         public int AnoDeCreacion { get; set; }
         public string Pais { get; set; }
@@ -12,19 +13,33 @@ namespace CoreEscuela.Entidades
         public TiposEscuela TipoEscuela { get; set; }
 
         public List<Curso> Cursos { get; set; }
+        string ILugar.Direccion { get; set; }
 
         //Constructor corto con asignacion por tuplas
         public Escuela(string nombre, int anoDeCreacion) => (Nombre, AnoDeCreacion) = (nombre, anoDeCreacion);
         //Constructor normal y con los ultimos 2 tipos de datos como opcionales
-        public Escuela(string nombre, int anoDeCreacion, TiposEscuela tipo, string pais = "", string ciudad = ""){
+        public Escuela(string nombre, int anoDeCreacion, TiposEscuela tipo, string pais = "", string ciudad = "")
+        {
             (Nombre, AnoDeCreacion) = (nombre, anoDeCreacion);//podemos reusar la asignacion por tuplas
             Pais = pais;
             Ciudad = ciudad;
         }
         //sobreescribimos el metodo toString de la clase object y le damos el comportamiento que queremos para este objeto en especifico
         //El signo e dolar $ antes del string le dice al compilador que dentro del string tendremos instrucciones
-        public override string ToString(){
+        public override string ToString()
+        {
             return $"Nombre: \"{Nombre}\"{System.Environment.NewLine}Tipo: \"{TipoEscuela}\"{System.Environment.NewLine}Ciudad: \"{Ciudad}\"";
+        }
+
+        public void LimpiarLugar()
+        {
+            Printer.DrawLine();
+            Console.WriteLine("Limpiando Escuela...");
+            foreach (var curso in Cursos)
+            {
+                curso.LimpiarLugar();
+            }
+            Console.WriteLine($"Escuela {Nombre} Limpia");
         }
     }
 }
